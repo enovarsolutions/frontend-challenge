f4e439fae4229c68d6f198ee04f2809173e9bce0
ryanlima <ryanlima08021997@gmail.com>
2021-02-21 15:21:16 -0300

    Adicionar Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "./style.css";
+// import logo from "../../assets/img/tudo-logo.png";
+
+import { ReactComponent as Notification } from "../../assets/svg/notifications-24px.svg";
+import { ReactComponent as ArrowDown } from "../../assets/svg/keyboard_arrow_down-24px.svg";
+
+const Header = () => {
+  return (
+    <div
+      id="header-container"
+      className="d-flex justify-content-between bg-black"
+    >
+      <h1 className="p-3 mb-auto">Logo</h1>
+      <div className="pt-5 flex-grow-1 text-center text-white">
+        <h4>Olá, José Carlos</h4>
+        <p className="mb-0">Seu crédito disponivel é de</p>
+        <h2 className="text-pink font-weight-bold">R$ 5.048,10</h2>
+        <div className="arrow-container mt-4 pt-2 ">
+          <div className="arrow-content rounded-circle mx-auto bg-pink">
+            <ArrowDown fill="white" width="40px" height="40px" />
+          </div>
+        </div>
+      </div>
+      <button className="btn p-3 mb-auto">
+        <Notification fill="white" width="30px" height="30px" />
+      </button>
+    </div>
+  );
+};
+
+export default Header;
