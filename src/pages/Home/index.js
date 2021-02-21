cc356f546326981ae76951f196a334ace6ce7719
ryanlima <ryanlima08021997@gmail.com>
2021-02-21 15:20:34 -0300

    configura pages

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,13 @@
+import React from "react";
+import Header from "../../components/Header";
+import Main from "../../components/Main";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Header com flex box</h1>
-    </div>
+    <>
+      <Header />
+      <Main />
+    </>
   );
 };
 
