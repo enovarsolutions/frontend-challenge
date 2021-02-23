6b2216933857c2e4663ccd4cf36655a35b489bd4
Eudalio Sousa <tonnydallios@gmail.com>
2021-02-22 23:21:38 -0300

    drawn home screen

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,59 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, StatusBar, Image, useWindowDimensions } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+import Logo from '../../assets/tudo-logo2.png';
+import CreditCard from '../../assets/creditCard.png';
+import Portabilidade from '../../assets/portability.png';
+import Refinanciamento from '../../assets/refinancing.png';
+import NovoEmprestimo from '../../assets/newLoan.png';
 
 // import { Container } from './styles';
 
 const Home = () => {
+  const width = useWindowDimensions().width;
+  const height = useWindowDimensions().height;
   return (
     <View style={styles.container}>
+        <StatusBar backgroundColor="#000" />
         <View style={styles.header}>
-          <Text style={styles.client}>Olá, José Carlos</Text>
-          <Text style={styles.text}>Seu crédito disponível é de</Text>
-          <Text style={styles.credito}>R$ 5.048,10</Text>
+          <View style={styles.headerIcons}>
+            <Image source={Logo} style={{ height: 40, width: 40 }} />
+            <Icon name="notifications-outline" size={30} color="#fff" />
+          </View>
+          <View>
+            <Text style={styles.client}>Olá, Eudálio Sousa</Text>
+            <Text style={styles.text}>Seu crédito disponível é de</Text>
+            <Text style={styles.credito}>R$ 5.048,10</Text>
+          </View>
+          <Icon style={styles.arrowBtn} name="chevron-down-circle" size={40} color="#d2267f" />
         </View>
-        <View>
-
+        <View style={styles.main}>
+          <View>
+            <Text style={styles.mainTitle}>Oportunidades</Text>
+          </View>
+          <View style={styles.menu}>
+              <View style={styles.menuItem}>
+                <Image source={NovoEmprestimo} style={{ height: 70, width: 70 }} />
+                <Text style={styles.textButton}>Novo Empréstimo</Text>
+                <Text style={styles.textItemValue}>Até R$ 3058,10</Text>
+              </View>
+              <View style={styles.menuItem}>
+                <Image source={Portabilidade} style={{ height: 70, width: 70 }} />
+                <Text style={styles.textButton}>Portabilidade</Text>
+                <Text style={styles.textItemValue}>Até R$ 2000,00</Text>
+              </View>
+              <View style={styles.menuItem}>
+                <Image source={Refinanciamento} style={{ height: 70, width: 70 }} />
+                <Text style={styles.textButton}>Refinanciamento</Text>
+                <Text style={styles.textItemValue}></Text>
+              </View>
+              <View style={styles.menuItem}>
+                <Image source={CreditCard} style={{ height: 70, width: 70 }} />
+                <Text style={styles.textButton}>Cartão de crédito consignado</Text>
+                <Text style={styles.textItemValue}></Text>
+              </View>
+          </View>
         </View>
     </View>
   );
@@ -26,27 +67,71 @@ const styles = StyleSheet.create({
   header: {
     backgroundColor: '#000',
     fontWeight: 'bold',
-    height: 210,
+    height: 200,
     borderBottomRightRadius: 50,
-    justifyContent: 'center',
+  },
+  headerIcons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 10,
+    paddingTop: 15
   },
   client: {
     color: '#fff',
-    fontSize: 25,
+    fontSize: 22,
     textAlign: 'center',
-    textAlignVertical: 'center'
+    marginBottom: 20,
+    fontWeight: 'bold'
   },
   text: {
     color: '#ccc',
-    fontSize: 22,
+    fontSize: 16,
     textAlign: 'center',
-    textAlignVertical: 'center'
+    textAlignVertical: 'center',
   },
   credito: {
     color: '#d2267f',
-    fontSize: 30,
+    fontSize: 33,
     textAlign: 'center',
-    textAlignVertical: 'center'
+    textAlignVertical: 'center',
+    fontWeight: 'bold'
+  },
+  arrowBtn: {
+    marginTop: 10,
+    marginHorizontal: '45%',
+  },
+  main: {
+    flex: 1,
+    paddingVertical: 35,
+    paddingHorizontal: 20,
+  },
+  mainTitle: {
+    color: '#000',
+    fontSize: 18,
+    marginBottom: 8,
+    fontWeight: 'bold'
+  },
+  menu: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  },
+  menuItem: {
+    width: 180,
+    height: 180,
+    borderRadius: 20,
+    marginBottom: 20,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  textButton: {
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  textItemValue: {
+    color: '#d22688',
+    fontSize: 12
   }
 });
 
