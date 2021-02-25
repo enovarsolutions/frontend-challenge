17dcc7bab4c788500cbdf8b0ac0cf71b3e59a734
ryanlima <ryanlima08021997@gmail.com>
2021-02-24 22:16:50 -0300

    Ajusta componente main

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -8,25 +8,25 @@ import creditCard from "../../assets/img/creditCard.png";
 
 const Main = () => {
   return (
-    <div className="container px-0">
-      <h3 className=" pt-5 pb-0">Oportunidades</h3>
+    <div className="container px-3">
+      <h4 className=" pt-5 pb-0">Oportunidades</h4>
       <div className="row">
         <ItemMain
           icon={newLoan}
           description="Novo Empréstimo"
           subdescription="Até R$ 3.058,10"
-          route="newloan"
+          route="newloan/values"
         />
         <ItemMain
           icon={portability}
           description="Portabilidade"
           subdescription="Até R$ 2.000,00"
-          route="portability"
+          route="newloan/portability"
         />
         <ItemMain
           icon={refinancing}
           description="Refinanciamento"
-          route="refinancing"
+          route="newloan/refinancing"
           disabled
         />
         <ItemMain
