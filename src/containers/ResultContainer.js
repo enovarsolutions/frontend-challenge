6e3c342cde6a27e4ad1124ae6d88aa9d06a85a33
ryanlima <ryanlima08021997@gmail.com>
2021-02-26 14:43:29 -0300

    Configura redux no ResultContainer

--- src/containers/ResultContainer.js
+++ src/containers/ResultContainer.js
@@ -1,12 +1,40 @@
 import React from "react";
 
+import { useDispatch, useSelector } from "react-redux";
+
+import { fetchResult, resultSelector, getError } from "../store/slices/result";
+
 import Result from "../pages/Loan/views/Result";
 
+import { resultCalc } from "../helpers/calcHelper";
 const ResultContainer = ({ setPrevPage }) => {
+  const dispatch = useDispatch();
+
+  const { result } = useSelector(resultSelector);
+
+  const { contractValue, installmentsValue, numberOfInstallments } = result;
+
   React.useEffect(() => {
     setPrevPage("/newloan/period");
   });
-  return <Result />;
+
+  React.useEffect(() => {
+    dispatch(fetchResult());
+  }, [dispatch]);
+
+  const interest = resultCalc(
+    installmentsValue,
+    contractValue,
+    numberOfInstallments
+  );
+  return (
+    <Result
+      numberOfInstallments={numberOfInstallments}
+      installmentsValue={installmentsValue}
+      contractValue={contractValue}
+      interest={interest}
+    />
+  );
 };
 
 export default ResultContainer;
